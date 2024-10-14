048912b6d0ddbaa0cd8aa41ffadfb6e94bb3a7ae
Stina <stina.marie.hansen@hotmail.com>
2024-10-14 16:23:21 +0200

    remove unserviceable customers
    Refactoring, forced customers to at least have something to order. remove customers when the bakery does not have what they want anymore.
    
    Next time: refactor so we got defined values at the top, with max/min customers and pastries

--- DTA400Project.py
+++ DTA400Project.py
@@ -3,8 +3,11 @@ import random
 
 menu = [["Cinnamonbun", 0], ["Chocolatechip cookie", 0], ["Blueberry pie", 0]]
 customers_in_queue = []
+customers_index_who_wants_to_leave = []
 #customers_gave_up = 0
-#customers_served = 0
+#customers_in_total = 0
+pastries_sold = 0
+missed_pastry_selling_opportunity = 0
 
 class Bakery(object):
     def __init__(self, env, num_cashiers):
@@ -15,6 +18,7 @@ class Bakery(object):
         for item in menu:
             item[1] = random.randint(5, 10) #bake between 5-10 of pastries
         print("Bakery baked", menu)
+        self.daily_batch = menu # remove if we cannot make an "exit function" when the simulation ends
 
         
     def purchase_pastry(self, current_customer):
@@ -24,44 +28,68 @@ class customer(object): #make a function??
     def __init__(self, env):
         self.env = env
         self.order = []
-        for item in menu:
-            self.order.append([item[0], random.randint(0,2)])
+
+        num_pastries = 0
+        for item in menu: #randomize order
+            wanted_amount = random.randint(0,3)
+            self.order.append([item[0], wanted_amount]) 
+            num_pastries += wanted_amount
+        if num_pastries < 1: #make sure the customer wants at least 1 thing
+            print("Customer wanted NOTHING, forced them to pick something")
+            self.order[random.randint(0,len(menu)-1)][1] = random.randint(1,3)
+
         customers_in_queue.append(self)
 
     
 #def spawn_customer():
+def update_menu():
+    for i in range(len(menu)):
+        menu[i][1] -= customers_in_queue[0].order[i][1]
+
+def detect_unserviceable_customers():
+    #if nothing the customer wants is in stock, leave
+    for customer_index in range(len(customers_in_queue)):
+        for pastry in range(len(menu)):
+            if customers_in_queue[customer_index].order[pastry][1] > 0: # if we even want this pastry...
+                if menu[pastry][1] > 0: #if the pastry is in stock, break and move on to the next customer check
+                    break
+            if pastry == len(menu) - 1: #if we are checking the last pastry and there is still nothing in the bakery the customer wants: prepare to leave
+                print("Customer wants",customers_in_queue[customer_index].order, "\nbut bakery only has", menu, ". Customer leaves.\n")
+                update_menu()
+                customers_index_who_wants_to_leave.append(customer_index)
+
+def remove_unserviceable_customers():
+    last_index = len(customers_index_who_wants_to_leave) - 1 #start from the back so the index cannot end up too big
+    for sad_customer in range(len(customers_index_who_wants_to_leave)):
+        customers_in_queue.pop(customers_index_who_wants_to_leave[last_index])
+        last_index -= 1
+
     
 def main(env, num_cashiers):
     bakery = Bakery(env, num_cashiers)
     
     #customers arriving when bakery opens
-    for i in range(random.randint(1,2)):
+    for i in range(random.randint(6,20)):
         newCustomer = customer(env)
     
     #debug: what the customers want (in order)
-    for currentCustomer in customers_in_queue:
-        print(currentCustomer.order)
-        
+    for customer_index in range(len(customers_in_queue)):
+        print("Customer", customer_index + 1, "wants", customers_in_queue[customer_index].order)
+    print("\n")
     while len(customers_in_queue) > 0: #or when event is still going, in case there is no queue
         yield env.timeout(random.randint(1, 3)) #customer buys pastries
         
+        print("There are", len(customers_in_queue), "customer left in the queue")
         #update menu
-        for i in range(len(menu)):
-            menu[i][1] -= customers_in_queue[0].order[i][1]
-        customers_in_queue.pop(0)
-        #customers_served += 1
+        print("Menu before purchase:", menu)
+        update_menu()
+        customers_in_queue.pop(0) #first customer in queue is done, leaving
+        print("Menu after purchase:", menu, "\n")
 
-        #customers in queue leave if everything they want is not available
-        for customer_index in range(len(customers_in_queue)):
-            for i in range(len(menu)):
-                if customers_in_queue[customer_index].order[i][1] > menu[i][1]: #kicka ut customer om allt den vill ha inte finns (kan ändra sen)
-                    print("Customer wants",customers_in_queue[customer_index].order[i], "but bakery only has", menu[i])
-                    customers_in_queue.pop(customer_index)
-                    #customers_gave_up += 1
-                    break
-
+        detect_unserviceable_customers()
+        remove_unserviceable_customers()
 
 
 env = simpy.Environment()
 env.process(main(env, 1)) #insert start function here
-env.run(until=10)  
+env.run(until=10)  #end when either time over or every part of menu is <= 0
